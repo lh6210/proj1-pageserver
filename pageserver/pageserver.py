b4b3b484da9d88b6b521c2596d8fcbe54e1c3dcb
hang.li <hang@debian.practice.com>
2017-10-02 10:11:20 -0700

    2

--- pageserver/pageserver.py
+++ pageserver/pageserver.py
@@ -92,7 +92,7 @@ def respond(sock):
 
     parts = request.split()
     if len(parts) > 1 and parts[0] == "GET":
-        """transmit(STATUS_OK, sock)"""
+        transmit(STATUS_OK, sock)
         transmit(STATUS_NOT_FOUND, sock)
     else:
         log.info("Unhandled request: {}".format(request))
